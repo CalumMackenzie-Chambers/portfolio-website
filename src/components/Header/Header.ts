a6f9388d2530115053d4fc217651eab6f02bea18
Calum <calum@mackenzie-chambers.co.uk>
2023-04-21 18:25:25 +0100

    style: format js and ts with 2 space tabs

--- src/components/Header/Header.ts
+++ src/components/Header/Header.ts
@@ -1,99 +1,99 @@
 // Function to toggle hamburger icon
 export const toggleHamburgerIcon = (bar1: HTMLElement, bar2: HTMLElement, bar3: HTMLElement) => {
-    bar1.classList.toggle("transform");
-    bar1.classList.toggle("-rotate-45");
-    bar1.classList.toggle("top-1/2");
+  bar1.classList.toggle("transform");
+  bar1.classList.toggle("-rotate-45");
+  bar1.classList.toggle("top-1/2");
 
-    bar2.classList.toggle("opacity-0");
+  bar2.classList.toggle("opacity-0");
 
-    bar3.classList.toggle("transform");
-    bar3.classList.toggle("rotate-45");
-    bar3.classList.toggle("top-1/2");
-    bar3.classList.toggle("top-6");
+  bar3.classList.toggle("transform");
+  bar3.classList.toggle("rotate-45");
+  bar3.classList.toggle("top-1/2");
+  bar3.classList.toggle("top-6");
 };
 
 // Function to close the mobile menu
 export const closeMenu = (body: HTMLElement, menu: HTMLElement) => {
-    body.classList.remove("overflow-hidden");
-    menu.classList.remove("menu-open");
-    menu.classList.add("menu-closed");
+  body.classList.remove("overflow-hidden");
+  menu.classList.remove("menu-open");
+  menu.classList.add("menu-closed");
 };
 
 // Function to reset the hamburger icon
 export const resetHamburgerIcon = (bar1: HTMLElement, bar2: HTMLElement, bar3: HTMLElement) => {
-    bar1.classList.remove("transform", "-rotate-45", "top-1/2");
-    bar2.classList.remove("opacity-0");
-    bar3.classList.remove("transform", "rotate-45", "top-1/2");
-    bar3.classList.add("top-6");
+  bar1.classList.remove("transform", "-rotate-45", "top-1/2");
+  bar2.classList.remove("opacity-0");
+  bar3.classList.remove("transform", "rotate-45", "top-1/2");
+  bar3.classList.add("top-6");
 };
 
 // Function to set the theme
 export const setTheme = (
-    theme: string,
-    htmlElement: HTMLElement,
-    toggleLg: HTMLInputElement,
-    toggleMobile: HTMLInputElement,
+  theme: string,
+  htmlElement: HTMLElement,
+  toggleLg: HTMLInputElement,
+  toggleMobile: HTMLInputElement,
 ) => {
-    if (theme === "dark") {
-        htmlElement.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-    } else {
-        htmlElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-    }
+  if (theme === "dark") {
+    htmlElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    htmlElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
 
-    toggleLg.checked = theme === "dark";
-    toggleMobile.checked = theme === "dark";
+  toggleLg.checked = theme === "dark";
+  toggleMobile.checked = theme === "dark";
 };
 
 // Function to initialize event listeners
 export const initEventListeners = () => {
-    const body = document.querySelector("body") as HTMLElement;
-    const menuButton = document.getElementById("hamburger-menu") as HTMLElement;
-    const menu = document.querySelector("nav") as HTMLElement;
+  const body = document.querySelector("body") as HTMLElement;
+  const menuButton = document.getElementById("hamburger-menu") as HTMLElement;
+  const menu = document.querySelector("nav") as HTMLElement;
 
-    const bar1 = document.getElementById("bar1") as HTMLElement;
-    const bar2 = document.getElementById("bar2") as HTMLElement;
-    const bar3 = document.getElementById("bar3") as HTMLElement;
+  const bar1 = document.getElementById("bar1") as HTMLElement;
+  const bar2 = document.getElementById("bar2") as HTMLElement;
+  const bar3 = document.getElementById("bar3") as HTMLElement;
 
-    const toggleLg = document.querySelector("#darkmode-toggle-lg") as HTMLInputElement;
-    const toggleMobile = document.querySelector("#darkmode-toggle-mobile") as HTMLInputElement;
-    const htmlElement = document.documentElement;
+  const toggleLg = document.querySelector("#darkmode-toggle-lg") as HTMLInputElement;
+  const toggleMobile = document.querySelector("#darkmode-toggle-mobile") as HTMLInputElement;
+  const htmlElement = document.documentElement;
 
-    menuButton.addEventListener("click", () => {
-        toggleHamburgerIcon(bar1, bar2, bar3);
-        body.classList.toggle("overflow-hidden");
-        menu.classList.toggle("menu-closed");
-        menu.classList.toggle("menu-open");
+  menuButton.addEventListener("click", () => {
+    toggleHamburgerIcon(bar1, bar2, bar3);
+    body.classList.toggle("overflow-hidden");
+    menu.classList.toggle("menu-closed");
+    menu.classList.toggle("menu-open");
+  });
+
+  document.querySelectorAll(".nav-link").forEach((navLink) => {
+    navLink.addEventListener("click", () => {
+      closeMenu(body, menu);
+      resetHamburgerIcon(bar1, bar2, bar3);
     });
+  });
 
-    document.querySelectorAll(".nav-link").forEach((navLink) => {
-        navLink.addEventListener("click", () => {
-            closeMenu(body, menu);
-            resetHamburgerIcon(bar1, bar2, bar3);
-        });
-    });
+  window.addEventListener("resize", () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 1024) {
+      closeMenu(body, menu);
+      resetHamburgerIcon(bar1, bar2, bar3);
+    }
+  });
 
-    window.addEventListener("resize", () => {
-        const screenWidth = window.innerWidth;
-        if (screenWidth < 1024) {
-            closeMenu(body, menu);
-            resetHamburgerIcon(bar1, bar2, bar3);
-        }
-    });
+  const savedTheme = localStorage.getItem("theme") || "light";
+  setTheme(savedTheme, htmlElement, toggleLg, toggleMobile);
 
-    const savedTheme = localStorage.getItem("theme") || "light";
-    setTheme(savedTheme, htmlElement, toggleLg, toggleMobile);
+  toggleMobile.addEventListener("change", () => {
+    const theme = toggleMobile.checked ? "dark" : "light";
+    setTheme(theme, htmlElement, toggleLg, toggleMobile);
+  });
 
-    toggleMobile.addEventListener("change", () => {
-        const theme = toggleMobile.checked ? "dark" : "light";
-        setTheme(theme, htmlElement, toggleLg, toggleMobile);
-    });
-
-    toggleLg.addEventListener("change", () => {
-        const theme = toggleLg.checked ? "dark" : "light";
-        setTheme(theme, htmlElement, toggleLg, toggleMobile);
-    });
+  toggleLg.addEventListener("change", () => {
+    const theme = toggleLg.checked ? "dark" : "light";
+    setTheme(theme, htmlElement, toggleLg, toggleMobile);
+  });
 };
 
 // Initialize event listeners on DOMContentLoaded
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,29 +3,29 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        colors: {
-            transparent: "transparent",
-            current: "currentColor",
-            black: colors.black,
-            white: colors.white,
-            gray: colors.gray,
-            orange: colors.orange,
-            green: colors.emerald,
-            purple: colors.indigo,
-            red: colors.rose,
-            yellow: colors.yellow,
-        },
-        fontFamily: {
-            sans: ["Montserrat", "sans-serif"],
-            serif: ["Lora", "serif"],
-        },
-        screens: {
-            xs: "475px",
-            ...defaultTheme.screens,
-        },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      orange: colors.orange,
+      green: colors.emerald,
+      purple: colors.indigo,
+      red: colors.rose,
+      yellow: colors.yellow,
     },
-    darkMode: "class",
-    plugins: [],
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Lora", "serif"],
+    },
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+  },
+  darkMode: "class",
+  plugins: [],
 };
