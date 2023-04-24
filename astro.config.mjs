4d43c6b728ec4650ee17e26ee8950ba784c5024e
Calum <calum@mackenzie-chambers.co.uk>
2023-04-24 05:11:17 +0100

    feat: add about section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), image({ serviceEntryPoint: "@astrojs/image/sharp" })],
 });
